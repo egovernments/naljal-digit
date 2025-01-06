f1b1ce3d3091fe8947e91c7ec4c9614e482cf5dc
Hari-eGov <hariprasad@egovernments.org>
2025-01-06 15:42:51 +0530

    Refactor and Common code added

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const webpack = require("webpack");
 module.exports = {
   // mode: 'development',
   entry: './src/index.js',
